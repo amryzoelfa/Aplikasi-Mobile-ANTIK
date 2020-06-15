57a9ed4d21ca5e5f28e853321f138e027914f096
ratna712 <ratnadsafitri@gmail.com>
2020-06-15 15:34:52 +0700

    layout ganti password

--- app/src/main/java/com/example/antik/Util/ServerAPI.java
+++ app/src/main/java/com/example/antik/Util/ServerAPI.java
@@ -5,5 +5,6 @@ public class ServerAPI {
     public static final String URL_ANTRIAN = "http://192.168.43.34/CIANTIK/Api/Antrian";
     public static final String URL_INFO = "http://192.168.43.34/CIANTIK/Api/Info";
     public static final String URL_DOKTER = "http://192.168.43.34/CIANTIK/Api/Dokter";
+    public static final String URL_GANTI_PASS = "http://192.168.43.34/CIANTIK/Api/gantipass";
 }
 
