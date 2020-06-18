f60eee2ea450130935a6d8d5ff9f2ae808b848ec
ratna712 <ratnadsafitri@gmail.com>
2020-06-18 13:59:21 +0700

    update riwayat

--- app/src/main/java/com/example/antik/RiwayatFragment.java
+++ app/src/main/java/com/example/antik/RiwayatFragment.java
@@ -4,15 +4,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.squareup.picasso.Picasso;
+
 public class RiwayatFragment extends Fragment {
+    private static final String Tanggal = "Tanggal";
+    private static final String Poli = "Poli";
+    private static final String Diagnosa = "Diagnosa";
+    private static final String Tindakan = "Tindakan";
+    private static final String Obat = "Obat";
+
+    TextView tanggal , poli, diagnosa, tindakan, obat;
+    String mtanggal, mpoli, mdiagnosa, mtindakan, mobat;
+    public static RiwayatFragment newInstance(String TGL, String POLI, String DIAGNOSA, String TINDAKAN, String OBAT){
+        RiwayatFragment fragment = new RiwayatFragment();
+        Bundle args = new Bundle();
+        args.putString(Tanggal, TGL);
+        args.putString(Poli, POLI);
+        args.putString(Diagnosa, DIAGNOSA);
+        args.putString(Tindakan, TINDAKAN);
+        args.putString(Obat, OBAT);
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mtanggal = getArguments().getString(Tanggal);
+            mpoli = getArguments().getString(Poli);
+            mdiagnosa = getArguments().getString(Diagnosa);
+            mtindakan = getArguments().getString(Tindakan);
+            mobat = getArguments().getString(Obat);
+        }
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_riwayat,container,false);
+        View v = inflater.inflate(R.layout.fragment_profil,container,false);
+
+        tanggal = v.findViewById(R.id.txttanggal);
+        poli = v.findViewById(R.id.txtpoli);
+        diagnosa = v.findViewById(R.id.txtdiagnosa);
+        tindakan = v.findViewById(R.id.txttindakan);
+        obat = v.findViewById(R.id.txtobat);
+
+        tanggal.setText(mtanggal);
+        poli.setText(mpoli);
+        diagnosa.setText(mdiagnosa);
+        tindakan.setText(mtindakan);
+        obat.setText(mobat);
+
+        Toast.makeText(getActivity(), mpoli+mdiagnosa+mtindakan+mobat, Toast.LENGTH_LONG).show();
+
+        return  v;
     }
 }
--- app/src/main/java/com/example/antik/Util/ServerAPI.java
+++ app/src/main/java/com/example/antik/Util/ServerAPI.java
@@ -1,7 +1,7 @@
 package com.example.antik.Util;
 
 public class ServerAPI {
-    public static final String URL_RIWAYAT = "http://192.168.43.34/CIANTIK/Api/Riwayat";
+    public static final String URL_RIWAYAT = "http://192.168.43.97/CIANTIK/Api/Riwayat";
     public static final String URL_ANTRIAN = "http://192.168.43.34/CIANTIK/Api/Antrian";
     public static final String URL_INFO = "http://192.168.43.34/CIANTIK/Api/Info";
     public static final String URL_DOKTER = "http://192.168.43.34/CIANTIK/Api/Dokter";
