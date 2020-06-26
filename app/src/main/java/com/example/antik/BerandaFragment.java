67f6b2d6969ae4502e5d67a003b5d29adfd815a2
Amry Zulfa Al Husna <amryzoelfa@gmail.com>
2020-06-26 14:18:35 +0700

    Update

--- app/src/main/java/com/example/antik/BerandaFragment.java
+++ app/src/main/java/com/example/antik/BerandaFragment.java
@@ -14,6 +14,8 @@ public class BerandaFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment_beranda, container, false);
+
+
     }
 }
 
--- app/src/main/java/com/example/antik/Util/ServerAPI.java
+++ app/src/main/java/com/example/antik/Util/ServerAPI.java
@@ -1,10 +1,13 @@
 package com.example.antik.Util;
 
 public class ServerAPI {
-    public static final String URL_RIWAYAT = "http://192.168.43.97/CIANTIK/Api/Riwayat";
+    public static final String URL_RIWAYAT = "http://192.168.43.34/CIANTIK/Api/Riwayat";
+    public static final String  URL_LOGIN = "http://192.168.43.34/CIANTIK/Api/LoginApi";
     public static final String URL_ANTRIAN = "http://192.168.43.34/CIANTIK/Api/Antrian";
     public static final String URL_INFO = "http://192.168.43.34/CIANTIK/Api/Info";
     public static final String URL_DOKTER = "http://192.168.43.34/CIANTIK/Api/Dokter";
     public static final String URL_GANTI_PASS = "http://192.168.43.34/CIANTIK/Api/gantipass";
+
+    public String getURL_LOGIN() { return URL_LOGIN; }
 }
 
