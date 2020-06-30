db8666149df59517f0737043f36f894e54a254fe
ratna712 <ratnadsafitri@gmail.com>
2020-06-30 20:54:21 +0700

    view antrian umum dan gigi, server

--- app/src/main/java/com/example/antik/Util/ServerAPI.java
+++ app/src/main/java/com/example/antik/Util/ServerAPI.java
@@ -1,12 +1,25 @@
 package com.example.antik.Util;
 
 public class ServerAPI {
-    public static final String URL_RIWAYAT = "http://192.168.43.34/CIANTIK/Api/Riwayat";
-    public static final String  URL_LOGIN = "http://192.168.43.34/CIANTIK/Api/LoginApi";
-    public static final String URL_ANTRIAN = "http://192.168.43.34/CIANTIK/Api/Antrian";
-    public static final String URL_INFO = "http://192.168.43.34/CIANTIK/Api/Info";
-    public static final String URL_DOKTER = "http://192.168.43.34/CIANTIK/Api/Dokter";
-    public static final String URL_GANTI_PASS = "http://192.168.43.34/CIANTIK/Api/gantipass";
+    public static final String URL_RIWAYAT = "http://antik.mif-project.com/Api/Riwayat/";
+    public static final String PROFIL_URL = "http://antik.mif-project.com/Api/Profile/";
+    private String URL_LOGIN = "http://antik.mif-project.com/Api/LoginApi";
+    public static final String URL_ANTRIAN = "http://antik.mif-project.com/Api/Antrian";
+    public static final String URL_INFO = "http://antik.mif-project.com/Api/Info";
+    public static final String URL_DOKTER = "http://antik.mif-project.com/Api/Dokter";
+    public static final String URL_GANTI_PASS = "http://antik.mif-project.com/Api/gantipass";
+    public static final String UPDATE_PROFIL = "http://antik.mif-project.com/Api/updateProfil/";
+    public static final String RUBAH_PASS = "http://antik.mif-project.com/Api/Password/";
+    public static final String INSERT_UMUM = "http://antik.mif-project.com/Api/insertUmum/";
+    public static final String INSERT_GIGI = "http://antik.mif-project.com/Api/insertGigi/";
+    public static final String ANTRIAN_UMUM= "http://antik.mif-project.com/Api/antrianUmum";
+    public static final String ANTRIAN_GIGI= "http://antik.mif-project.com/Api/antrianGigi";
+    public static final String JUMLAH_UMUM= "http://antik.mif-project.com/Api/getJumum";
+    public static final String JUMLAH_GIGI= "http://antik.mif-project.com/Api/getJGigi";
+    public static final String LIST_GIGI= "http://antik.mif-project.com/Api/listGigi";
+    public static final String LIST_UMUM= "http://antik.mif-project.com/Api/listUmum";
+    public static final String CETAK_UMUM = "http://antik.mif-project.com/Api/cetakUmum/";
+    public static final String CETAK_GIGI = "http://antik.mif-project.com/Api/cetakGigi/";
 
     public String getURL_LOGIN() { return URL_LOGIN; }
 }
