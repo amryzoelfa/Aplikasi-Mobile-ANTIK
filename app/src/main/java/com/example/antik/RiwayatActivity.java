3809d8c65ba1c52b9bcb7ea49490f6f8cfb8bfd0
wulancahya22 <wulancahya562@gmail.com>
2020-07-01 05:03:24 +0700

    riwayat

---
+++ app/src/main/java/com/example/antik/RiwayatActivity.java
@@ -0,0 +1,112 @@
+package com.example.antik;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RiwayatActivity extends AppCompatActivity {
+	RecyclerView mRecyclerview;
+	RecyclerView.Adapter mAdapter;
+	RecyclerView.LayoutManager mManager;
+	List<ModelRiwayat> mItems;
+	ProgressDialog pd;
+	SessionManager sessionManager;
+
+	private String mId_user, mNama;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_riwayat);
+
+		sessionManager = new SessionManager(this);
+		sessionManager.checkLogin();
+		HashMap<String, String> user = sessionManager.getUserDetail();
+		mId_user = user.get(sessionManager.ID_USER);
+		mNama = user.get(sessionManager.NAMA);
+
+		mRecyclerview = (RecyclerView) findViewById(R.id.recyclerviewRiwayat);
+		pd = new ProgressDialog(RiwayatActivity.this);
+		mItems = new ArrayList<>();
+
+		loadJson();
+
+		mManager = new LinearLayoutManager(RiwayatActivity.this,LinearLayoutManager.VERTICAL,false);
+		mRecyclerview.setLayoutManager(mManager);
+		mAdapter = new AdapterRiwayat(RiwayatActivity.this,mItems);
+		mRecyclerview.setAdapter(mAdapter);
+
+	}
+
+	private void loadJson()
+	{
+		pd.setMessage("Mengambil Data");
+		pd.setCancelable(false);
+		pd.show();
+
+		String url = ServerAPI.URL_RIWAYAT + mId_user;
+
+		JsonArrayRequest reqData = new JsonArrayRequest(Request.Method.POST, url,null,
+			new Response.Listener<JSONArray>() {
+				@Override
+				public void onResponse(JSONArray response) {
+					pd.cancel();
+					Log.d("volley","response : " + response.toString());
+					for(int i = 0 ; i < response.length(); i++)
+					{
+						try {
+							JSONObject data = response.getJSONObject(i);
+							ModelRiwayat md = new ModelRiwayat();
+							md.setTanggal(data.getString("tanggal"));
+							md.setPoli(data.getString("ket_poli"));
+							md.setDiagnosa(data.getString("diagnosa"));
+							md.setTindakan(data.getString("tindakan"));
+							md.setObat(data.getString("resep_obat"));
+							mItems.add(md);
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+					}
+					mAdapter.notifyDataSetChanged();
+				}
+			},
+			new Response.ErrorListener() {
+				@Override
+				public void onErrorResponse(VolleyError error) {
+					pd.cancel();
+					Log.d("volley", "error : " + error.getMessage());
+				}
+			})
+		{
+			@Override
+			protected Map<String, String> getParams() throws AuthFailureError {
+				Map<String, String> params = new HashMap<>();
+				params.put("id_user", mId_user);
+				params.put("nama", mNama);
+				return params;
+			}
+		};
+
+		AppController.getInstance().addToRequestQueue(reqData);
+	}
+
+}
