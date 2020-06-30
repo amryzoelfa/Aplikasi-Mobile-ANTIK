5bb7c3fb5fe5191e3395803991b47d0d3bbb5299
Amry Zulfa Al Husna <amryzoelfa@gmail.com>
2020-06-30 21:29:47 +0700

    List Antrian

--- app/src/main/java/com/example/antik/AntrianGigiActivity.java
+++ app/src/main/java/com/example/antik/AntrianGigiActivity.java
@@ -1,14 +1,100 @@
 package com.example.antik;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.antik.Adapter.AdapterListGigi;
+import com.example.antik.Model.ModelListGigi;
+import com.example.antik.Util.AppController;
+import com.example.antik.Util.ServerAPI;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class AntrianGigiActivity extends AppCompatActivity {
+    RecyclerView mRecyclerview;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mManager;
+    List<ModelListGigi> mItems;
+    ProgressDialog pd;
+    SessionManager sessionManager;
+
+    private String mId_user, mNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_antrian_gigi);
+
+        sessionManager = new SessionManager(this);
+        sessionManager.checkLogin();
+        HashMap<String, String> user = sessionManager.getUserDetail();
+        mId_user = user.get(sessionManager.ID_USER);
+
+        mRecyclerview = (RecyclerView) findViewById(R.id.recyclerviewGigi);
+        pd = new ProgressDialog(AntrianGigiActivity.this);
+        mItems = new ArrayList<>();
+
+        loadJson();
+
+        mManager = new LinearLayoutManager(AntrianGigiActivity.this,LinearLayoutManager.VERTICAL,false);
+        mRecyclerview.setLayoutManager(mManager);
+        mAdapter = new AdapterListGigi(AntrianGigiActivity.this,mItems);
+        mRecyclerview.setAdapter(mAdapter);
+
+    }
+
+    private void loadJson()
+    {
+        pd.setMessage("Mengambil Data");
+        pd.setCancelable(false);
+        pd.show();
+
+        JsonArrayRequest reqData = new JsonArrayRequest(Request.Method.POST, ServerAPI.LIST_GIGI,null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        pd.cancel();
+                        Log.d("volley","response : " + response.toString());
+                        for(int i = 0 ; i < response.length(); i++)
+                        {
+                            try {
+                                JSONObject data = response.getJSONObject(i);
+                                ModelListGigi md = new ModelListGigi();
+                                //md.setTanggal(data.getString("tanggal"));
+                                md.setNo_antrian(data.getString("no_antrian"));
+                                md.setNama(data.getString("nama"));
+                                md.setKet_status(data.getString("ket_status"));
+                                mItems.add(md);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        mAdapter.notifyDataSetChanged();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pd.cancel();
+                        Log.d("volley", "error : " + error.getMessage());
+                    }
+                });
+
+        AppController.getInstance().addToRequestQueue(reqData);
     }
 }
--- app/src/main/java/com/example/antik/AntrianUmumActivity.java
+++ app/src/main/java/com/example/antik/AntrianUmumActivity.java
@@ -1,14 +1,101 @@
 package com.example.antik;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.antik.Adapter.AdapterListUmum;
+import com.example.antik.Model.ModelListUmum;
+import com.example.antik.Util.AppController;
+import com.example.antik.Util.ServerAPI;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class AntrianUmumActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerview;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mManager;
+    List<ModelListUmum> mItems;
+    ProgressDialog pd;
+    SessionManager sessionManager;
+
+    private String mId_user, mNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_antrian_umum);
+
+        sessionManager = new SessionManager(this);
+        sessionManager.checkLogin();
+        HashMap<String, String> user = sessionManager.getUserDetail();
+        mId_user = user.get(sessionManager.ID_USER);
+
+        mRecyclerview = (RecyclerView) findViewById(R.id.recyclerviewUmum);
+        pd = new ProgressDialog(AntrianUmumActivity.this);
+        mItems = new ArrayList<>();
+
+        loadJson();
+
+        mManager = new LinearLayoutManager(AntrianUmumActivity.this,LinearLayoutManager.VERTICAL,false);
+        mRecyclerview.setLayoutManager(mManager);
+        mAdapter = new AdapterListUmum(AntrianUmumActivity.this,mItems);
+        mRecyclerview.setAdapter(mAdapter);
+
+    }
+
+    private void loadJson()
+    {
+        pd.setMessage("Mengambil Data");
+        pd.setCancelable(false);
+        pd.show();
+
+        JsonArrayRequest reqData = new JsonArrayRequest(Request.Method.POST, ServerAPI.LIST_UMUM,null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        pd.cancel();
+                        Log.d("volley","response : " + response.toString());
+                        for(int i = 0 ; i < response.length(); i++)
+                        {
+                            try {
+                                JSONObject data = response.getJSONObject(i);
+                                ModelListUmum md = new ModelListUmum();
+                                //md.setTanggal(data.getString("tanggal"));
+                                md.setNo_antrian(data.getString("no_antrian"));
+                                md.setNama(data.getString("nama"));
+                                md.setKet_status(data.getString("ket_status"));
+                                mItems.add(md);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        mAdapter.notifyDataSetChanged();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pd.cancel();
+                        Log.d("volley", "error : " + error.getMessage());
+                    }
+                });
+
+        AppController.getInstance().addToRequestQueue(reqData);
     }
 }
